aa0d40597d6867aee6c66743d4ee08f024c015d5
码力 <mali@opc.ai>
2026-06-07 10:37:48 +0800

    @fix(评审): 6-11条优惠券可核销+会员中心充值支付+真名+眼睛

--- prototype/customer-mp/pages/member-center/member-center.js
+++ prototype/customer-mp/pages/member-center/member-center.js
@@ -1,1 +1,84 @@
-const API = require('../../utils/api');Page({data:{user:{},balance:0,totalSpent:0,visitCount:0},onShow(){const user=API.getCurrentUser();if(user){this.setData({user,balance:user.balance||0,totalSpent:user.totalSpent||0,visitCount:user.visitCount||0});API.getBalance().then(balance=>this.setData({balance}))}},goMyOrders(){wx.switchTab({url:'/pages/my-orders/my-orders'})},goMyCoupons(){wx.navigateTo({url:'/pages/my-coupons/my-coupons'})},goTopUp(){wx.showToast({title:'充值功能开发中',icon:'none'})},goRoomControl(){wx.navigateTo({url:'/pages/room-control/room-control'})}})
+var API = require('../../utils/api')
+
+Page({
+  data: {
+    userName: '会员', levelLabel: '普通会员',
+    balance: 0, balanceDisplay: '¥0', balanceVisible: true,
+    totalSpent: 0, visitCount: 0,
+    selectedAmount: 100,
+    showPaymentModal: false,
+    selectedPayment: 'WeChat'
+  },
+
+  onShow: function() {
+    var self = this
+    var user = API.getCurrentUser()
+    if (user) {
+      var levels = { Normal:'普通会员', Silver:'银卡会员', Gold:'金卡会员', Diamond:'钻石会员' }
+      // 第10条：显示真实姓名
+      var displayName = user.name || user.nickname || user.phone || '会员'
+      self.setData({
+        userName: displayName,
+        levelLabel: levels[user.memberLevel] || '普通会员',
+        totalSpent: user.totalSpent || 0,
+        visitCount: user.visitCount || 0
+      })
+    }
+    API.getBalance().then(function(b) {
+      self.setData({ balance: b || 0 })
+      self.updateBalanceDisplay()
+    })
+  },
+
+  updateBalanceDisplay: function() {
+    if (this.data.balanceVisible) {
+      this.setData({ balanceDisplay: '¥' + this.data.balance })
+    } else {
+      this.setData({ balanceDisplay: '****' })
+    }
+  },
+
+  // 第33条/第4条：隐藏/显示余额
+  toggleBalance: function() {
+    this.setData({ balanceVisible: !this.data.balanceVisible })
+    this.updateBalanceDisplay()
+  },
+
+  goOrders: function() { wx.navigateTo({ url: '/pages/my-orders/my-orders' }) },
+  goCoupons: function() { wx.navigateTo({ url: '/pages/my-coupons/my-coupons' }) },
+
+  selectAmount: function(e) {
+    this.setData({ selectedAmount: parseInt(e.currentTarget.dataset.amount) })
+  },
+
+  // 第9条：支付方式选择
+  doRecharge: function() {
+    this.setData({ showPaymentModal: true, selectedPayment: 'WeChat' })
+  },
+
+  selectPayment: function(e) {
+    this.setData({ selectedPayment: e.currentTarget.dataset.payment })
+  },
+
+  cancelPayment: function() {
+    this.setData({ showPaymentModal: false })
+  },
+
+  confirmRecharge: function() {
+    var self = this
+    this.setData({ showPaymentModal: false })
+    wx.showLoading({ title: '充值中...' })
+    API.topUp(self.data.selectedAmount, self.data.selectedPayment).then(function(r) {
+      wx.hideLoading()
+      wx.showToast({
+        title: '✅ 充值成功！¥' + r.amount + (r.bonus > 0 ? ' (赠送¥' + r.bonus + ')' : ''),
+        icon: 'none', duration: 2000
+      })
+      self.setData({ balance: r.newBalance })
+      self.updateBalanceDisplay()
+    }).catch(function(err) {
+      wx.hideLoading()
+      wx.showToast({ title: err.message || '充值失败', icon: 'none' })
+    })
+  }
+})
--- prototype/customer-mp/pages/my-coupons/my-coupons.js
+++ prototype/customer-mp/pages/my-coupons/my-coupons.js
@@ -1,2 +1,26 @@
-const API = require('../../utils/api')
-Page({ data: { coupons: [] }, onLoad() { API.getUserCoupons().then(coupons => this.setData({ coupons })) } })
+var API = require('../../utils/api')
+
+Page({
+  data: { coupons: [] },
+  onLoad: function() {
+    var self = this
+    API.getUserCoupons().then(function(coupons) { self.setData({ coupons: coupons }) })
+  },
+
+  // 第6-7条：点击优惠券可引导消费/核销
+  useCoupon: function(e) {
+    var ds = e.currentTarget.dataset
+    if (ds.used === 'true') { wx.showToast({ title: '该券已使用', icon: 'none' }); return }
+    wx.showModal({
+      title: '使用优惠券',
+      content: ds.desc + '\n价值 ¥' + ds.value + '\n是否前往验券？',
+      success: function(res) {
+        if (res.confirm) {
+          wx.navigateTo({ url: '/pages/coupon-verify/coupon-verify?code=' + ds.code })
+        }
+      }
+    })
+  },
+
+  goBack: function() { wx.navigateBack() }
+})
