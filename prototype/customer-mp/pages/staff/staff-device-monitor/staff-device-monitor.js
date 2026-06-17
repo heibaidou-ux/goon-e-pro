0b3eb29e5f30cd1458e5f5f3a784abb263272863
码力 <mali@opc.ai>
2026-06-17 10:10:43 +0800

    @fix(device-monitor): 房间选择改为居中弹窗+设备名加房间前缀
    1. 设备监控页的房间筛选器由原生picker改为居中自定义弹窗
    2. 设备名称改为 "房间名+设备类型" 格式(如"大茶室C 灯光")
    3. 弹窗样式统一居中(不再底部弹出)

--- prototype/customer-mp/pages/staff/staff-device-monitor/staff-device-monitor.js
+++ prototype/customer-mp/pages/staff/staff-device-monitor/staff-device-monitor.js
@@ -1,20 +1,62 @@
-﻿const API = require('../../../utils/staff-api')
+const STAFF_API = require('../../../utils/staff-api')
+const API = require('../../../utils/api')
 Page({
-  data: { rooms: [], devices: [], stats: { total: 0, online: 0, rate: '0%' }, selectedRoomId: '', selectedRoomName: '全部房间', roomOptions: ['全部房间'] },
+  data: {
+    rooms: [], devices: [], stats: { total: 0, online: 0, rate: '0%' },
+    selectedRoomId: '', selectedRoomName: '全部房间', roomOptions: ['全部房间'],
+    showRoomPicker: false
+  },
+
   onShow() {
-    Promise.all([API.getRoomStatusList(), API.getDeviceList(), API.getDeviceStats()]).then(([rooms, devices, stats]) => {
-      const names = rooms.map(r => r.name)
-      this.setData({ rooms, devices, stats, roomOptions: ['全部房间', ...names] })
+    var self = this
+    Promise.all([STAFF_API.getRoomStatusList(), STAFF_API.getDeviceList(), STAFF_API.getDeviceStats()]).then(function(results) {
+      var rooms = results[0] || [], devices = results[1] || [], stats = results[2] || {}
+      var names = rooms.map(function(r) { return r.name })
+      // 构建房间名称到ID的映射
+      var roomMap = {}
+      for (var i = 0; i < rooms.length; i++) roomMap[rooms[i].name] = rooms[i].roomId
+      // 给设备加上房间名
+      devices = self._enrichDeviceNames(devices, roomMap)
+      self.setData({ rooms: rooms, devices: devices, stats: stats, roomOptions: ['全部房间'].concat(names) })
     })
   },
-  onRoomChange(e) {
-    const idx = e.detail.value
-    const name = this.data.roomOptions[idx]
+
+  _enrichDeviceNames: function(devices, roomMap) {
+    var labels = { Lock:'门锁', AC:'空调', Light:'灯光', Curtain:'窗帘', Speaker:'音响', Fan:'风扇', BGM:'音响' }
+    var icons = { Lock:'🔒', AC:'❄️', Light:'💡', Curtain:'🪟', Speaker:'🔊', Fan:'🌀', BGM:'🎵' }
+    for (var i = 0; i < devices.length; i++) {
+      var d = devices[i]
+      var roomName = ''
+      for (var r in roomMap) { if (roomMap[r] === d.roomId) { roomName = r; break } }
+      d.typeLabel = (roomName ? roomName + ' ' : '') + (labels[d.type] || d.type)
+      d.typeIcon = icons[d.type] || '📡'
+    }
+    return devices
+  },
+
+  // ── 居中房间选择弹窗 ──
+  showRoomPicker: function() { this.setData({ showRoomPicker: true }) },
+  hideRoomPicker: function() { this.setData({ showRoomPicker: false }) },
+
+  selectRoom: function(e) {
+    var idx = parseInt(e.currentTarget.dataset.idx)
+    var self = this
+    this.setData({ showRoomPicker: false })
+    var name = this.data.roomOptions[idx]
     if (idx === 0) {
-      API.getDeviceList().then(devices => this.setData({ devices, selectedRoomId: '', selectedRoomName: name }))
+      STAFF_API.getDeviceList().then(function(devices) {
+        var roomMap = {}
+        for (var i = 0; i < self.data.rooms.length; i++) roomMap[self.data.rooms[i].name] = self.data.rooms[i].roomId
+        devices = self._enrichDeviceNames(devices, roomMap)
+        self.setData({ devices: devices, selectedRoomId: '', selectedRoomName: name })
+      })
     } else {
-      const room = this.data.rooms[idx - 1]
-      API.getDeviceList(room.roomId).then(devices => this.setData({ devices, selectedRoomId: room.roomId, selectedRoomName: name }))
+      var room = self.data.rooms[idx - 1]
+      STAFF_API.getDeviceList(room.roomId).then(function(devices) {
+        var roomMap = {}; roomMap[room.name] = room.roomId
+        devices = self._enrichDeviceNames(devices, roomMap)
+        self.setData({ devices: devices, selectedRoomId: room.roomId, selectedRoomName: name })
+      })
     }
   }
 })
